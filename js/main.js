e7bcd8556a7b1655514a6764c4cec73d25f68522
critanaro <critanaro@gmail.com>
2021-04-14 01:13:13 -0500

    changed everything lol temporarily

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
   function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("19 September 2019 0:00:00 CST+00:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
